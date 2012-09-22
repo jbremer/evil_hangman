f04baaa4270737c9e39b6244713d2dc4587cf1d5
Jurriaan Bremer <jurriaanbremer@gmail.com>
2012-09-22 19:17:48 +0200

    bare EvilHangman app

---
+++ EvilHangman/EhAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EhAppDelegate.h
+//  EvilHangman
+//
+//  Created by Jurriaan Bremer on 9/22/12.
+//  Copyright (c) 2012 Lolcat, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EhAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EvilHangman/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EvilHangman
+//
+//  Created by Jurriaan Bremer on 9/22/12.
+//  Copyright (c) 2012 Lolcat, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EhAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EhAppDelegate class]));
+    }
+}
