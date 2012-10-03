c9cd9b982d02449cfd8ccf19fc85b30530c01e08
Jurriaan Bremer <jurriaanbremer@gmail.com>
2012-10-03 10:18:40 +0200

    improved dictionary code a bit

--- EvilHangman/Dictionary.h
+++ EvilHangman/Dictionary.h
@@ -15,14 +15,14 @@
 {
     /*Contains the word that should be printed, Has _ instead of letter
      When the letter is not yet guessed. */
-    char * userword;
+    char userword[100];
     
     /* The difficulty is used to distinguish between 3 modes,
      Namely Evil (2), Semi-evil (1) and honest (0). Default = 1*/
     int difficulty;
     
     //Null terminated string with already guessed letters
-    char * guessedletters;
+    char guessedletters[27];
     
     @private
     NSMutableArray * dict;
--- EvilHangman/Dictionary.m
+++ EvilHangman/Dictionary.m
@@ -10,114 +10,107 @@
 
 @implementation Dictionary
 
-
-- (id)init {
+-(id)init {
     self = [super init];
-    if (self) {
+    if(self) {
         difficulty = 1;
-        userword = calloc(1, sizeof(char)*100);
-        guessedletters = calloc(1, sizeof(char)*27);
         dict = [NSMutableArray arrayWithCapacity:100];
         possiblewords = [NSMutableArray arrayWithCapacity:100];
+
+        //Initialize the Dictionary @TODO pthreads
+        FILE *fp = fopen("words", "r");
+        char next[100];
+        while (fgets(next, 100, fp)) {
+            [dict addObject:[NSString stringWithUTF8String:next]];
+        }
+        fclose(fp);
     }
-    
-    //Initialize the Dictionary @TODO pthreads
-    FILE * words = fopen("r", "words");
-    unsigned long maxwordlength = 100;
-    char next[maxwordlength]; 
-    while ((fgets(next, maxwordlength, words))) {
-        [dict addObject:[NSString stringWithUTF8String:next]];
-    }
-    
     return self;
 }
 
-- (void) initWord {
-    int i;
-    
+-(void)initWord {
     //Clean everything
-    guessedletters[0] = '\0';
+    guessedletters[0] = 0;
     [possiblewords removeAllObjects];
-    
+
     //Initialize everything
     int index = arc4random() % [dict count];
-    NSString * ourword = [dict objectAtIndex:index];
-    int length = [ourword length];
-    
-    for (i = 0; i < length; i++) {
-        userword[i]='_';
+    NSString *ourword = [dict objectAtIndex:index];
+
+    userword[ourword.length] = 0;
+    for (int i = 0; i < ourword.length; i++) {
+        userword[i] = '_';
     }
-    userword[i]='\0';
-    
+
     //Honest mode implementation is easypeasy
-    if (difficulty==0) {
+    if(difficulty == 0) {
         [possiblewords addObject:ourword];
-    } else {
-        for (NSString * word in dict) {
-            if ([word length] == length)
+    }
+    else {
+        for (NSString *word in dict) {
+            if(word.length == ourword.length) {
                 [possiblewords addObject:word];
+            }
         }
     }
 }
 
-- (bool) guessLetter:(char)letter {
-    char letterstring[2] = {0};
-    letterstring[0] = letter;
+-(bool)guessLetter:(char)letter {
+    char letterstring[2] = {letter, 0};
     int notfound = 0;
-    
+
     strcat(guessedletters, letterstring);
-    
+
     //check for a word that is not yet found and replace ourword
-    for (NSString * word in possiblewords) {
-        if ([word rangeOfString:[NSString stringWithUTF8String:letterstring]].location
-                == NSNotFound) {        
+    for (NSString *word in possiblewords) {
+        if([word rangeOfString:[NSString stringWithUTF8String:letterstring]].location == NSNotFound) {
             notfound = 1;
             break;
         }
     }
-    
+
     //Remove all words with the letter if we have at least one entry left without the letter
-    if (notfound) {
+    if(notfound) {
         for (int i = 0; i < [possiblewords count]; i++) {
-            if ([[possiblewords objectAtIndex:i] rangeOfString:[NSString 
+            if([[possiblewords objectAtIndex:i] rangeOfString:[NSString
                     stringWithUTF8String:letterstring]].location != NSNotFound) {
                 [possiblewords removeObjectAtIndex:i];
             }
         }
         return false;
-        
-    } else {
-        
+    }
+    else {
+
         //Choose a word to use if we must fill in the letter @TODO not random?
-        NSString * ourword = [possiblewords objectAtIndex:arc4random() % [possiblewords count]];
-        int indices[10] = {0};
-        int j = 0;
-        
+        NSString *ourword = [possiblewords objectAtIndex:arc4random() % [possiblewords count]];
+        int indices[10] = {0}, j = 0;
+
         //Fill it and remember where
-        for (int i = 0; i < [ourword length] ; i++) {
-            if ([ourword characterAtIndex:i] == letter) {
+        for (int i = 0; i < ourword.length; i++) {
+            if([ourword characterAtIndex:i] == letter) {
                 userword[i] = letter;
-                indices[j] = i;
-                j++;
+                indices[j++] = i;
             }
         }
-        
+
         //Remove words that dont have the characters at the same place
         for (int i = 0; i < [possiblewords count]; i++) {
             NSString * word = [possiblewords objectAtIndex:i];
             for (int k = j ; k < j ; k++) {
-                if (![word characterAtIndex:indices[k]] == letter) {
+                if([word characterAtIndex:indices[k]] != letter) {
                     [possiblewords removeObjectAtIndex:i];
                 }
             }
         }
-        
+
         //Check if the user guessed the whole word
         for (int i = 0 ; i < strlen(userword); i++) {
-            if (userword[i] == '_') return false;
+            if(userword[i] == '_') {
+                return false;
+            }
         }
         return true;
     }
 }
-    
+
 @end
