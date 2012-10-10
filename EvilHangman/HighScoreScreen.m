53b9a8c593517d36304dfe7d20d612e27d6a0f95
Lion User <User@Lions-Mac.local>
2012-10-10 23:33:10 +0200

    progress but bugzord

--- EvilHangman/HighScoreScreen.m
+++ EvilHangman/HighScoreScreen.m
@@ -13,10 +13,10 @@
 - (id)init {
     self = [super init];
     if (self) {
-        //[self faggotry];
-        NSUserDefaults *prefs;
-        prefs = [NSUserDefaults standardUserDefaults];
-        NSString *Score = [prefs stringForKey:@"scores"];
+        [self addraw];
+        [self add:@"Hanz",300];
+        NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
+        NSString *Score = [prefs objectForKey:@"scores"];
         
         CGRect screenRect = [[UIScreen mainScreen] bounds];
         CGFloat screenWidth = screenRect.size.width;
@@ -26,7 +26,7 @@
         title.textAlignment = UITextAlignmentCenter;
         title.font = [UIFont systemFontOfSize:25];
         
-        UILabel *playerlist = [[UILabel alloc] initWithFrame:CGRectMake(0, 50, screenWidth, 500)];
+        UILabel *playerlist = [[UILabel alloc] initWithFrame:CGRectMake(0, 50, screenWidth, 80)];
         playerlist.numberOfLines = 0;
         playerlist.textAlignment = UITextAlignmentCenter;
         
@@ -40,10 +40,57 @@
 
 
 // Saving some random stuff to NSUserdefaults
-- (void)faggotry{
-    NSUserDefaults * prefs;
-    NSString *durr = @"Henk\t12";
-    [prefs setString: durr forKey:@"scores"];
+- (bool) add : (NSString *)name, int score {
+    NSUserDefaults * prefs = [NSUserDefaults standardUserDefaults];
+    NSString *oldscore = [prefs objectForKey:@"scores"];
+    NSMutableString *newscore = [oldscore mutableCopy];
+    NSCharacterSet *tabsnenters = [NSCharacterSet characterSetWithCharactersInString:@"\n\t"];
+    NSArray * strings = [oldscore componentsSeparatedByCharactersInSet: tabsnenters];
+    int added = 0;
+    for (int i = 0; i+added < 10; i++) {
+        // if old score length has not been reached
+        if (i*2+1 < [strings count]) {
+            // if score in args not added yet and is higher than score at current index: add arg and current value if new scorelist is not full
+            if (added == 0 && score > (int)[strings objectAtIndex:i*2+1]) {
+                [newscore appendString:name];
+                [newscore appendString:@"\t"];
+                [newscore appendString:(NSString *)score];
+                [newscore appendString:@"\n"];
+                added = 1;
+                if (i+added < 9) {
+                    [newscore appendString:[strings objectAtIndex:i*2]];
+                    [newscore appendString:@"\t"];
+                    [newscore appendString:[strings objectAtIndex:i*2+1]];
+                    [newscore appendString:@"\n"];
+                }
+            // otherwise add score at next index
+            } else {
+                [newscore appendString:[strings objectAtIndex:i*2]];
+                [newscore appendString:@"\t"];
+                [newscore appendString:[strings objectAtIndex:i*2+1]];
+                [newscore appendString:@"\n"];
+            }
+        // if all scores have been looped through but the score given with the arg has not been added yet: add
+        } else if (added == 0 && (int)[strings objectAtIndex:i*2+1] != 0) {
+            [newscore appendString:name];
+            [newscore appendString:@"\t"];
+            [newscore appendString:(NSString *)score];
+            [newscore appendString:@"\n"];
+            added = 1;
+            break;
+        }
+        
+    }
+    if (added == 0) {
+        [prefs setObject: newscore forKey:@"scores"];
+        return true;
+    } else return false;
+}
+
+- (void) addraw {
+    NSUserDefaults * prefs = [NSUserDefaults standardUserDefaults];
+    NSString *scorez = @"Henk\t1337\nHerp\t500\nDerp\t100";
+    [prefs setObject: scorez forKey:@"scores"];
 }
 
 @end
