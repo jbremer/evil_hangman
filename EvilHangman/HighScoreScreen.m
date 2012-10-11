e1d777cb37b11304823411b235b3829530b69b14
Kas van 't Veer <kas@itslabss-MacBook.local>
2012-10-11 13:30:11 +0200

    ITS ALLLIIIIIVE (but kinda raw n haxy)

--- EvilHangman/HighScoreScreen.m
+++ EvilHangman/HighScoreScreen.m
@@ -13,8 +13,8 @@
 - (id)init {
     self = [super init];
     if (self) {
-        [self addraw];
-        [self add:@"Hanz",300];
+        [self unittest:@"Henkie" , 42];
+        
         NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
         NSString *Score = [prefs objectForKey:@"scores"];
         
@@ -26,7 +26,7 @@
         title.textAlignment = UITextAlignmentCenter;
         title.font = [UIFont systemFontOfSize:25];
         
-        UILabel *playerlist = [[UILabel alloc] initWithFrame:CGRectMake(0, 50, screenWidth, 80)];
+        UILabel *playerlist = [[UILabel alloc] initWithFrame:CGRectMake(0, 50, screenWidth, 300)];
         playerlist.numberOfLines = 0;
         playerlist.textAlignment = UITextAlignmentCenter;
         
@@ -41,9 +41,12 @@
 
 // Saving some random stuff to NSUserdefaults
 - (bool) add : (NSString *)name, int score {
+    if ([name rangeOfString:@"\n"].location != NSNotFound &&
+        [name rangeOfString:@"\t"].location != NSNotFound)
+        return false;
     NSUserDefaults * prefs = [NSUserDefaults standardUserDefaults];
     NSString *oldscore = [prefs objectForKey:@"scores"];
-    NSMutableString *newscore = [oldscore mutableCopy];
+    NSMutableString *newscore = [@"" mutableCopy];
     NSCharacterSet *tabsnenters = [NSCharacterSet characterSetWithCharactersInString:@"\n\t"];
     NSArray * strings = [oldscore componentsSeparatedByCharactersInSet: tabsnenters];
     int added = 0;
@@ -51,10 +54,10 @@
         // if old score length has not been reached
         if (i*2+1 < [strings count]) {
             // if score in args not added yet and is higher than score at current index: add arg and current value if new scorelist is not full
-            if (added == 0 && score > (int)[strings objectAtIndex:i*2+1]) {
+            if (added == 0 && score > [[strings objectAtIndex:i*2+1] intValue]) {
                 [newscore appendString:name];
                 [newscore appendString:@"\t"];
-                [newscore appendString:(NSString *)score];
+                [newscore appendString:[NSString stringWithFormat:@"%d",score]];
                 [newscore appendString:@"\n"];
                 added = 1;
                 if (i+added < 9) {
@@ -71,25 +74,47 @@
                 [newscore appendString:@"\n"];
             }
         // if all scores have been looped through but the score given with the arg has not been added yet: add
-        } else if (added == 0 && (int)[strings objectAtIndex:i*2+1] != 0) {
+        } else if (added == 0 && score != 0) {
             [newscore appendString:name];
             [newscore appendString:@"\t"];
-            [newscore appendString:(NSString *)score];
+            [newscore appendString:[NSString stringWithFormat:@"%d",score]];
             [newscore appendString:@"\n"];
             added = 1;
             break;
-        }
-        
+        } else break;
     }
-    if (added == 0) {
+    if (added == 1) {
         [prefs setObject: newscore forKey:@"scores"];
         return true;
     } else return false;
 }
 
-- (void) addraw {
+// Test the bunch by adding ten dummyplayers to the highscore and adding an eleventh with chosen name and score
+- (void) unittest : (NSString *) name, int score {
+    [self fillwithdummies];
+    if ([self eligible: score])
+        [self add: name, score];
+    
+}
+
+// Saving some random stuff to NSUserdefaults
+- (bool) eligible : (int) score {
     NSUserDefaults * prefs = [NSUserDefaults standardUserDefaults];
-    NSString *scorez = @"Henk\t1337\nHerp\t500\nDerp\t100";
+    NSString *oldscore = [prefs objectForKey:@"scores"];
+    NSCharacterSet *tabsnenters = [NSCharacterSet characterSetWithCharactersInString:@"\n\t"];
+    NSArray * strings = [oldscore componentsSeparatedByCharactersInSet: tabsnenters];
+    if ([strings count] < 21)
+        return true;
+    else if ([[strings objectAtIndex:19] intValue] < score)
+        return true;
+    else
+        return false;
+}
+
+// Fill the scores key in nsuserdefaults for testing purposes
+- (void) fillwithdummies {
+    NSUserDefaults * prefs = [NSUserDefaults standardUserDefaults];
+    NSString *scorez = @"one\t100\ntwo\t90\nthree\t80\nfour\t70\nfive\t60\nsix\t50\nseven\t40\neight\t30\nnine\t20\nten\t10\n";
     [prefs setObject: scorez forKey:@"scores"];
 }
 
